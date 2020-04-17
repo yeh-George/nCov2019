82bb4c4ef3762f8a51212a4d071fcfc543e34b3e
yeh-George <ycj012@163.com>
2020-04-17 08:56:50 +0800

    add requirements.txt, wsgi.py and ProductionConfig.

--- nCov2019/settings.py
+++ nCov2019/settings.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
 class BaseConfig:
     SECRET_KEY = os.getenv('SECRET_KEY', 'secret key')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///' + os.path.join(basedir, 'data.db'))
 
     # bless分页
     NCOV2019_BLESS_PER_PAGE = 7
 
 
 class DevelopmentConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.getenv('sqlite:///' + os.path.join(basedir, 'data-dev.db'))
 
 
 class ProductionConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///' + os.path.join(basedir, 'data.db'))
 
 
 config = {
